271dae9854ee4b914c81a93b5395b014c0afcab4
Ivan Zhmur <zhmur.ivan@gmail.com>
2025-07-13 12:24:31 +0300

    feat(*): add building blocks; add own mediator

---
+++ src/BuidingBlocks/ActivityLog.SharedKernel/Mediator/Sender/ISender.cs
@@ -0,0 +1,8 @@
+﻿using ActivityLog.SharedKernel.Mediator.Request;
+
+namespace ActivityLog.SharedKernel.Mediator.Sender;
+
+public interface ISender
+{
+    Task<TResponse> Send<TResponse>(IRequest<TResponse> request, CancellationToken cancellationToken = default);
+}
